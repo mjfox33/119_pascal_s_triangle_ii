cf55abf9d21a54c8e886534d99f690589dbc4946
mjfox33 <mjfox33@gmail.com>
2022-02-01 20:50:20 -0500

    inital commit

---
+++ 119_pascal_s_triangle_ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        result = list()
+        for i in range(rowIndex+1):
+            row = list()
+            for j in range(i+1):
+                cell = 1 if j==0 or j == i else result[i-1][j-1]+result[i-1][j]
+                row.append(cell)
+            result.append(row)
+        return result[-1]
